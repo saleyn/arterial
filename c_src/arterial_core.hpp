887909c072c27071d9970fe23b423bf6055fd71d
Serge Aleynikov <saleyn@gmail.com>
2026-07-06 08:49:56 -0400

    Reformat code

--- c_src/arterial_core.hpp
+++ c_src/arterial_core.hpp
@@ -1,34 +1,32 @@
 #pragma once
 
+#include "arterial_connection.hpp"
 #include "enif.hpp"
 #include "throttle.hpp"
-#include "arterial_connection.hpp"
-#include <atomic>
-#include <array>
-#include <vector>
-#include <memory>
 #include <algorithm>
-#include <cstring>
-#include <type_traits>
-#include <unistd.h>
-#include <sys/uio.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
+#include <array>
+#include <atomic>
 #include <bit>
+#include <chrono>
+#include <cstring>
+#include <ctime>
 #include <errno.h>
 #include <fcntl.h>
-#include <cstring>
+#include <memory>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <type_traits>
 #include <unistd.h>
-#include <ctime>
-#include <chrono>
+#include <vector>
 
 #ifdef HAVE_OPENSSL
-#include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/opensslv.h>
+#include <openssl/ssl.h>
 #endif
 
 namespace arterial {
@@ -40,35 +38,31 @@ struct PoolStripe;
 
 // 1. The protocol dispatcher
 template <typename Visitor>
-void visit_protocol(ProtocolType proto, Visitor&& visitor) {
+void visit_protocol(ProtocolType proto, Visitor&& visitor)
+{
   switch (proto) {
-    case PROTO_SSL: 
-      visitor(std::integral_constant<ProtocolType, PROTO_SSL>{}); 
-      break;
-    case PROTO_UDP: 
-      visitor(std::integral_constant<ProtocolType, PROTO_UDP>{}); 
-      break;
-    default:        
-      visitor(std::integral_constant<ProtocolType, PROTO_TCP>{}); 
-      break;
+    case PROTO_SSL: visitor(std::integral_constant<ProtocolType, PROTO_SSL>{}); break;
+    case PROTO_UDP: visitor(std::integral_constant<ProtocolType, PROTO_UDP>{}); break;
+    default:        visitor(std::integral_constant<ProtocolType, PROTO_TCP>{}); break;
   }
 }
 
 // A tiny helper mapping struct
 struct ProtocolMap {
-    const char* name;
-    ProtocolType type;
+  const char*  name;
+  ProtocolType type;
 };
 
 // The dispatcher loop
 template <typename Visitor>
-auto visit_protocol_str(const char* protocol_str, Visitor&& visitor) {
+auto visit_protocol_str(const char* protocol_str, Visitor&& visitor)
+{
   // 1. Define the supported protocols in a clean data table
   static constexpr ProtocolMap mapping[] = {
-    {"tcp", PROTO_TCP},
-    {"udp", PROTO_UDP},
+      {"tcp", PROTO_TCP},
+      {"udp", PROTO_UDP},
 #ifdef HAVE_OPENSSL
-    {"ssl", PROTO_SSL},
+      {"ssl", PROTO_SSL},
 #endif
   };
 
@@ -90,7 +84,6 @@ auto visit_protocol_str(const char* protocol_str, Visitor&& visitor) {
   return visitor(std::integral_constant<ProtocolType, PROTO_UNKNOWN>{});
 }
 
-
 //=============================================================================
 // NIF Utility Functions
 //=============================================================================
@@ -98,16 +91,23 @@ auto visit_protocol_str(const char* protocol_str, Visitor&& visitor) {
 // Resolve {PoolRef, StripeId, SlotId} to a Connection&, or nullptr if any index is out of range.
 // This is used by handle_readable/3, handle_writable/3, close_slot/3, etc.
 inline Connection* resolve_slot(
-  ErlNifEnv* env, [[maybe_unused]] int argc, const ERL_NIF_TERM argv[], PoolContext** out_ctx);
+    ErlNifEnv* env, [[maybe_unused]] int argc, const ERL_NIF_TERM argv[], PoolContext** out_ctx);
 
 // Time spacing throttling check - returns true if the request was allowed
 inline bool throttle_allow(PoolContext* ctx, Connection& slot);
 
 // Return true, and parse an integer or boolean term into `value`
-inline bool get_int_or_bool_option(ErlNifEnv* env, ERL_NIF_TERM opt, int& value) {
+inline bool get_int_or_bool_option(ErlNifEnv* env, ERL_NIF_TERM opt, int& value)
+{
   if (get(env, opt, value)) return true;
-  if (enif_is_identical(opt, am_true))  { value = 1; return true; }
-  if (enif_is_identical(opt, am_false)) { value = 0; return true; }
+  if (enif_is_identical(opt, am_true)) {
+    value = 1;
+    return true;
+  }
+  if (enif_is_identical(opt, am_false)) {
+    value = 0;
+    return true;
+  }
   return false;
 }
 
@@ -116,13 +116,13 @@ inline bool get_int_or_bool_option(ErlNifEnv* env, ERL_NIF_TERM opt, int& value)
 //===========================================================================
 
 // Helper function to set socket options using string name matching
-template<typename SetOptFunc>
-inline bool set_sockopt_by_name(const char* opt_name, const char* target_name,
-                                int opt_value, SetOptFunc set_func) {
-  if (strcmp(opt_name, target_name) == 0) {
-    return set_func(opt_value) == 0;  // setsockopt returns 0 on success
-  }
-  return false;  // Name doesn't match - continue to next option
+template <typename SetOptFunc>
+inline bool set_sockopt_by_name(
+    const char* opt_name, const char* target_name, int opt_value, SetOptFunc set_func)
+{
+  if (strcmp(opt_name, target_name) == 0)
+    return set_func(opt_value) == 0; // setsockopt returns 0 on success
+  return false;                      // Name doesn't match - continue to next option
 }
 
 } // namespace arterial
@@ -136,18 +136,16 @@ namespace arterial {
 
 // Resolve {PoolRef, StripeId, SlotId} to a Connection&, or nullptr if any index is out of range
 inline Connection* resolve_slot(
-  ErlNifEnv* env, [[maybe_unused]] int argc, const ERL_NIF_TERM argv[], PoolContext** out_ctx)
+    ErlNifEnv* env, [[maybe_unused]] int argc, const ERL_NIF_TERM argv[], PoolContext** out_ctx)
 {
   PoolContext* ctx;
   unsigned int stripe_id, slot_id;
 
   assert(argc == 3);
 
-  if  (!get(env, argv[0], ctx)
-    || !ctx
-    || !get(env, argv[1], stripe_id, (unsigned int)(ctx->stripe_count-1))
-    || !ctx->stripes[stripe_id]
-    || !get(env, argv[2], slot_id)) [[unlikely]]
+  if (!get(env, argv[0], ctx) || !ctx ||
+      !get(env, argv[1], stripe_id, (unsigned int)(ctx->stripe_count - 1)) ||
+      !ctx->stripes[stripe_id] || !get(env, argv[2], slot_id)) [[unlikely]]
     return nullptr;
 
   auto& stripe = *ctx->stripes[stripe_id];
@@ -159,8 +157,9 @@ inline Connection* resolve_slot(
 }
 
 // Time spacing throttling check - returns true if the request was allowed
-inline bool throttle_allow(PoolContext* ctx, Connection& slot) {
-  return ctx->get_throttle_rate_per_sec() == 0                // No throttling configured
+inline bool throttle_allow(PoolContext* ctx, Connection& slot)
+{
+  return ctx->get_throttle_rate_per_sec() == 0          // No throttling configured
       || slot.throttle.add(1, arterial::now_utc()) > 0; // check if we can add one request
 }
 
